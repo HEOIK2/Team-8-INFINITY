828f5ef4cf62d31125c07d90e781542cecdc96d1
cyj4494-rgb <cyj4494@gmail.com>
2026-07-29 15:20:11 +0900

    #include 라이브러리 추가

--- textRPG/textRPG/main.cpp
+++ textRPG/textRPG/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+#include "main.h"
 
 int main() {
 	std::cout << "Welcome to the Text RPG!" << std::endl;
