b94efad653db719c174013e7a3b3d0a183c69584
HEO IK <heoikng@gmail.com>
2026-08-02 20:29:19 +0900

    feat: 함수 Init, Color, MakeGauge 추가

---
+++ Sorting Knight/Sorting Knight/test_main.cpp
@@ -0,0 +1,11 @@
+﻿#include "ui.h"
+#include <iostream>
+
+int main() {
+	UI_Init();
+	std::cout << Color("빨간색", "31") << "\n";
+	std::cout << "원래 색\n";
+	std::cout << MakeGauge(142, 200, 20) << "\n";
+	return 0;
+
+}
--- Sorting Knight/Sorting Knight/ui.h
+++ Sorting Knight/Sorting Knight/ui.h
@@ -1,1 +1,8 @@
-#pragma once
+﻿#pragma once					// 이 헤더가 한번만 포함되게 하는 문법. 여러 파일에서 이 헤더를 부르면 반복해서 불러서 개꼬이는 이상한 사태를 미연에 방지함. 그냥 헤더에는 쓴다고 기계적으로 외워라.
+#include <string>
+
+void UI_Init();													// Initialize 초기화. 이 넘은 콘솔이 ANSI 색상 코드를 해석하게 켜주는 것입니다. main 맨 앞에 딱 한번만 키면 돼요.
+std::string Color(const std::string& s, const char* code);		// string은 std에서 꺼내온 것이기 때문에 std. const 상수. 참조 받으면 원본 건드릴 수도 있으니까 읽기 전용을 명시하는 것
+
+std::string MakeGauge(int cur, int max, int slots);
+// HP바 문자열 생성하는 것. 예시 MakeGauge(142, 200, 20) -> 현재값, 최대값, 칸수 -> ■■■■■■■■■■■ 142/ 200
